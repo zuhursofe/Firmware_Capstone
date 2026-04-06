d00fcfb764e02e3e62d2e4bdf63d58c1c51e8921
zuhursofe <sofe.2@buckeyemail.osu.edu>
2026-04-06 19:49:13 -0400

    Initial Project Import

---
+++ ble5_simple_peripheral_cc2640r2lp_stack_library/FlashROM_Library/ROM/common_rom_init.d
@@ -0,0 +1,6 @@
+# FIXED
+
+ROM/common_rom_init.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/rom/r2/common_rom_init.c
+
+C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/rom/r2/common_rom_init.c:
+
