a7ee79fba4f0e98c8b5381155714b9c4e91cc839
zuhursofe <sofe.2@buckeyemail.osu.edu>
2026-04-09 17:27:07 -0400

    deleted wrong files..

--- ble5_simple_peripheral_cc2640r2lp_stack_library/FlashROM_Library/ROM/common_rom_init.d
+++
@@ -1,6 +0,0 @@
-# FIXED
-
-ROM/common_rom_init.obj: C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/rom/r2/common_rom_init.c
-
-C:/ti/simplelink_cc2640r2_sdk_5_30_00_03/source/ti/ble5stack/rom/r2/common_rom_init.c:
-
